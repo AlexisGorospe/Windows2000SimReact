853aab80bd5f61c3fb49905fd7de70abe9d1f6ec
AlexisGorospe <AlexisGorospe@github.com>
2025-02-03 00:03:57 -0500

    start menu and taskbar are now a part of app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useState, useRef } from "react";
+import Clock from "react-live-clock";
+
 import './App.css';
 
 //Sounds
 import snd_logon from "./assets/sounds/logon.wav"
 
-//icons
-import Icons from "./img/icons/style.css";
+import ico_iexplore from "./assets/img/icons/internet_explorer/internet_explorer.ico"
 
-import ico_iexplore from "./img/icons/internet_explorer/internet_explorer.ico"
+import ico_explorer from "./assets/img/icons/explorer/explorer.ico"
+import ico_my_computer from "./assets/img/icons/explorer/my_computer.ico"
+import ico_recycle_bin from "./assets/img/icons/explorer/recycle_bin.ico"
 
-import ico_explorer from "./img/icons/explorer/explorer.ico"
-import ico_my_computer from "./img/icons/explorer/my_computer.ico"
-import ico_recycle_bin from "./img/icons/explorer/recycle_bin.ico"
+import ico_msg_x from "./assets/img/icons/msg/msg_x.ico"
+import ico_msg_exclamation from "./assets/img/icons/msg/msg_exclamation.ico"
 
-import ico_msg_x from "./img/icons/msg/msg_x.ico"
-import ico_msg_exclamation from "./img/icons/msg/msg_exclamation.ico"
+import ico_mspaint from "./assets/img/icons/mspaint/mspaint.ico"
+import ico_mspaint_image from "./assets/img/icons/mspaint/mspaint_image.ico"
 
-import ico_mspaint from "./img/icons/mspaint/mspaint.ico"
-import ico_mspaint_image from "./img/icons/mspaint/mspaint_image.ico"
+import ico_notepad from "./assets/img/icons/notepad/notepad.ico"
 
-import ico_notepad from "./img/icons/notepad/notepad.ico"
 
-import "./assets/style/style_desktopIcons.css"
+//Start Menu
+import start_banner from "./assets/img/start_banner.png"
+import "./assets/style/style_startMenu.css"
 
 //Taskbar
-import Taskbar from "./components/taskbar/Taskbar";
-import './components/taskbar/style.css';
+import "./assets/style/style_taskbar.css"
 
 //Window
 import Window from "./components/windows/Window";
@@ -52,10 +53,15 @@ Draggable.propTypes = {
 
 const App = () => {
     //state
+
+    // Windows
     const [currentWindowId, setCurrentWindowId] = useState(0);
     const [windows , setWindows] = useState([]);
 
-    //functions
+    // taskbar and stuff
+    const [startMenuOpen, setStartMenuOpen] = useState(false);
+
+    // Window
     const createNewWindow = (titlebar = "Untitled - Notepad", program = 1) => {
         console.log(titlebar)
         console.log(program)
@@ -65,6 +71,12 @@ const App = () => {
             <Window key={prevWindows.length} titlebar={titlebar}program={program.toString()}/>
         ]);
         console.log(titlebar + program);
+    }
+
+    // Taskbar
+    const startClick = () => {
+        setStartMenuOpen(!startMenuOpen);
+        console.log(startMenuOpen);
     }
 
     const testfunction = () => {
@@ -115,7 +127,55 @@ const App = () => {
                 </button>
                 */}
             </div>
-            <Taskbar createNewWindow={createNewWindow}/>
+
+            <div id={"taskbar"}>
+                <div id={"taskbar_left_side"}>
+                    <button id={"startButton"} onClick={startClick} className={startMenuOpen ? 'pressed' : ''}/>
+                    <div className={"taskbar_separator"}></div>
+                    <div id={"quickLaunch"}>
+                        {/*
+                        {
+                            this.state.quickStartPrograms.map((program) => {
+                                return (
+                                    <div className={"quickStartProgram"} >
+
+                                    </div>
+                                )
+                            })
+                        }
+                        */}
+
+                        <img src={ico_iexplore} className={"quickLaunchProgram"} onClick={createNewWindow}/>
+                    </div>
+                    <div className={"taskbar_separator"}></div>
+                    <div id={"runningPrograms"}>
+
+                    </div>
+                </div>
+                <div id={"notificationArea"}>
+                    <div id={"notificationIcons"}>
+
+                    </div>
+                    <p id={"start_clock"}><Clock format={"HH:mm"} ticking={true}/></p>
+                </div>
+
+            </div>
+
+
+            <div id={"start_menu"} className={startMenuOpen ? 'visible' : ''}>
+                <p>{startMenuOpen}</p>
+                <img id={"start_logo_banner"} src={start_banner}/>
+                <div id={"start_menu_programs"}>
+                    <div className={"start_program_list"}>
+                        <button className={"start_program"} onClick={startClick}>
+                            <img src={ico_iexplore}/>
+                            <p className={"start_program_name"}>Internet Explorer</p>
+                        </button>
+                    </div>
+                    <div className={"start_menu_separator"}></div>
+                </div>
+            </div>
+
         </div>
     );
 }
