c0b276440ca0684b4ac6a4152114f4b82356e852
AlexisGorospe <AlexisGorospe@github.com>
2025-02-03 19:37:55 -0500

    added an unfinished dropdown menu in notepad

--- src/components/programs/notepad/Notepad.js
+++ src/components/programs/notepad/Notepad.js
@@ -1,12 +1,34 @@
 import React, {useState} from "react";
 
 function Notepad(props) {
+    const [fileDDM, setFileDDM] = useState(false)
+    const [textAreaContent, setTextAreaContent] =  useState("")
+
+    //DDM is short for drop-down menu
+    function showFileDDM(){
+        setFileDDM(!fileDDM)
+        console.log(fileDDM)
+    }
+
+    function setNewFile(){
+        // stuff to just erase the contents of the textarea
+    }
+    
     return (
         <div className={"program_notepad"}>
             <div className={"menubar"}>
                 {/*https://www.w3schools.com/howto/howto_js_dropdown.asp*/}
 
-                <button>File</button>
+                <div className="dropdown">
+                    <button className="dropbtn" onClick={showFileDDM}>File</button>
+                    <div id="myDropdown" class={fileDDM ? 'dropdown-content visible' : 'dropdown-content '}>
+                        <a href="#">New</a>
+                        <a href="#">Open</a>
+                        <a href="#">Save</a>
+                        <a href="#">Save As</a>
+                    </div>
+                </div>
+
                 <button>Edit</button>
                 <button>Format</button>
                 <button>Help</button>
