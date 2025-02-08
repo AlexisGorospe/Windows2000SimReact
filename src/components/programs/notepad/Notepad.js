e3b819df5b04cf513eb388b4eb1afa42d71720cc
AlexisGorospe <AlexisGorospe@github.com>
2025-02-08 17:51:43 -0500

    added a saving function to norepad

--- src/components/programs/notepad/Notepad.js
+++ src/components/programs/notepad/Notepad.js
@@ -1,8 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsText
+
+
+
 import React, {useState} from "react";
 
 function Notepad(props) {
+    // file
     const [fileDDM, setFileDDM] = useState(false)
     const [textAreaContent, setTextAreaContent] =  useState("")
+    const [file, setFile] = useState("");
 
     function handleTextAreaChange(event){
         setTextAreaContent(event.target.value)
@@ -13,11 +19,32 @@ function Notepad(props) {
     function showFileDDM(){
         setFileDDM(!fileDDM)
         console.log(fileDDM)
+        console.log(file)
     }
 
     function file_new(){
         setTextAreaContent("")
         setFileDDM(false)
+    }
+
+    function file_open(){
+        console.log("this function is supposed to ask the user for a text file they wanna open in this notepad sim")
+        
+        
+        // let popupWindow = window.open('url','windowName','options') //keeping this comment here because i think it's cool
+    }
+
+    function file_save(){
+        const blob = new Blob([textAreaContent], {type: "text/plain"})
+        const url = URL.createObjectURL(blob)
+        const link = document.createElement("a")
+        link.href = url
+        link.download = "Untitled.txt"
+        document.body.appendChild(link)
+        link.click()
+
+        document.body.removeChild(link)
+        URL.revokeObjectURL(url)
     }
     
     return (
@@ -29,11 +56,12 @@ function Notepad(props) {
                     <button className="dropbtn" onClick={showFileDDM}>File</button>
                     <div id="myDropdown" className={fileDDM ? 'dropdown-content visible' : 'dropdown-content '}>
                         <a onClick={file_new}>New</a>
-                        <a href="#">Open</a>
-                        <a href="#">Save</a>
+                        <a onClick={file_open}>Open</a>
+                        <a onClick={file_save}>Save</a>
                         <a href="#">Save As</a>
                     </div>
                 </div>
+
 
                 <button>Edit</button>
                 <button>Format</button>
