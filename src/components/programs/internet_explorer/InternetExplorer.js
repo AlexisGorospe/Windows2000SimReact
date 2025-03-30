4fddeec3180401a6ffcb718c3f6ee9a23a5a7415
AlexisGorospe <AlexisGorospe@github.com>
2025-03-30 18:12:22 -0400

    added search bar

--- src/components/programs/internet_explorer/InternetExplorer.js
+++ src/components/programs/internet_explorer/InternetExplorer.js
@@ -1,18 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.css"
 
 function InternetExplorer(){
 
+    const [currentUrl, setCurrentUrl] = useState("https://www.mojeek.com/")
+    const [searchBarContent, setSearchBarContent] = useState("") 
+
+    const search = (event) => {
+        event.preventDefault();
+
+        setCurrentUrl("https://www.bing.com/search?q=hello there")
+    }
+
+
     return (
         <div className={"internet_explorer"}>
             <header>
-                <div></div>
+                <form onSubmit={search}>
+                    <input type="text" value={searchBarContent}></input>
+                    <button type="submit" value={"replace this text later"}></button>
+                </form>
             </header>
-            <iframe src={"/"}/>
+            <iframe src={currentUrl}/>
         </div>
 
 
     )
 }
+
+/*
+list of search engines that work
+
+*/
 
 export default InternetExplorer;
